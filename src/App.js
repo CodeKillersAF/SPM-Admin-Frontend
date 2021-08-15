4f09d3f17cfa95e6aff75ad0a32e59f6d169a22b
TharushaDilmith <tharushadilmith99@gmail.com>
2021-08-16 00:42:58 +0530

    created the layout of page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 
-
+import {BrowserRouter, BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Layout from './components/layout/Layout';
+import Home from './pages/home/Home';
 function App() {
   return (
     <div className="App">
-      hello
+      <BrowserRouter>
+        <section>
+          <Switch>
+            <Layout>
+              <Route exact path="/" component={Home} />
+            </Layout>
+          </Switch>
+        </section>
+      </BrowserRouter>
     </div>
   );
 }
