1a4cf3442de5b7239915a01951da1021bec309a5
kawsikan <kawsinote@gmail.com>
2021-08-17 22:07:36 +0530

    create add ford file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 
-import {BrowserRouter, BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import { BrowserRouter, BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Layout from './components/layout/Layout';
 import Home from './pages/home/Home';
+import ViewSupplier from './pages/supplier/ViewSupplier';
 function App() {
   return (
     <div className="App">
@@ -10,6 +11,7 @@ function App() {
           <Switch>
             <Layout>
               <Route exact path="/" component={Home} />
+              <Route exact path="/supplier" component={ViewSupplier} />
             </Layout>
           </Switch>
         </section>
---
+++ src/pages/supplyItem/AddSupplyItem.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ViewSupplier() {
+    return (
+        <div>
+            View Suppliers
+        </div>
+    )
+}
