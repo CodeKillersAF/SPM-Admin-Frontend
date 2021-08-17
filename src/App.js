cb0692224a7dd74638e8539c3ecb3a3a925b1ded
Didula-Lakminda <didulalakminda450@gmail.com>
2021-08-17 23:02:47 +0530

    authentication set

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@
 import {BrowserRouter, BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Layout from './components/layout/Layout';
 import Home from './pages/home/Home';
+
+import AdminLoginPage from './pages/AdminLoginPage/Adminloginpage';
+
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <section>
           <Switch>
+
+          <Route exact path="/" component={AdminLoginPage} />
+
             <Layout>
-              <Route exact path="/" component={Home} />
+              <Route exact path="/home" component={Home} />
             </Layout>
           </Switch>
         </section>
