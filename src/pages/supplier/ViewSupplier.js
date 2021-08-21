a5a85a825527dc41e202ce0625b2508f18a0fd95
kawsikan <kawsinote@gmail.com>
2021-08-21 18:38:20 +0530

    delete function

--- src/pages/supplier/ViewSupplier.js
+++ src/pages/supplier/ViewSupplier.js
@@ -7,62 +7,18 @@ import DeleteIcon from "@material-ui/icons/Delete";
 import { Edit } from "@material-ui/icons";
 import Button from "@material-ui/core/Button";
 import { useHistory } from "react-router-dom";
-
-const columns = [
-    // { field: "_id", headerName: "ID", width: 160 },
-    {
-        field: "supplier_name",
-        headerName: "Name",
-        width: 150,
-        editable: false,
-    },
-    {
-        field: "email",
-        headerName: "Email",
-        width: 150,
-        editable: false,
-    },
-    {
-        field: "contact",
-        headerName: "Contact",
-        width: 150,
-        editable: false,
-    },
-    {
-        field: "address",
-        headerName: "Address",
-        width: 150,
-        editable: false,
-    },
-    {
-        field: "action",
-        headerName: "Action",
-        width: 200,
-        editable: true,
-        renderCell: (params) => {
-            return (
-                <>
-                    <Button
-                        variant="contained"
-                        color="primary"
-                        startIcon={<Edit />}
-                        style={{ marginLeft: "20px", marginRight: "30px" }}
-                    >
-                        Edit
-                    </Button>
-                    <DeleteIcon color="secondary" />
-                </>
-            );
-        },
-    },
-];
+import IconButton from "@material-ui/core/IconButton";
+import DialogBoxConfirm from "../../components/dialogBoxConfirm/DialogBoxConfirm";
 
 export default function ViewSupplier() {
+
     const [supplier, setsupplier] = useState([]);
+    const [open, setOpen] = React.useState(false);
+    const [tableID, setTableID] = useState("");
 
     useEffect(() => {
         getSupplier();
-    }, []);
+    }, [setOpen]);
 
     const getSupplier = () => {
         axios.get("http://localhost:8000/api/admin/supplier").then((res) => {
@@ -76,9 +32,85 @@ export default function ViewSupplier() {
         history.push("/addsupplier")
     }
 
+    const handleClose = () => {
+        setOpen(false);
+    };
+    const handleClickOpen = (tableID) => {
+        setTableID(tableID);
+        setOpen(true);
+    };
+
+    const onClickDelete = () => {
+        console.log(tableID);
+        axios
+            .delete("http://localhost:8000/api/admin/supplier/" + tableID)
+            .then((res) => {
+                console.log("deleted");
+                setOpen(false);
+            });
+    };
+
+    const columns = [
+        // { field: "_id", headerName: "ID", width: 160 },
+        {
+            field: "supplier_name",
+            headerName: "Name",
+            width: 150,
+            editable: false,
+        },
+        {
+            field: "email",
+            headerName: "Email",
+            width: 150,
+            editable: false,
+        },
+        {
+            field: "contact",
+            headerName: "Contact",
+            width: 150,
+            editable: false,
+        },
+        {
+            field: "address",
+            headerName: "Address",
+            width: 150,
+            editable: false,
+        },
+        {
+            field: "action",
+            headerName: "Action",
+            width: 200,
+            editable: true,
+            renderCell: (params) => {
+                return (
+                    <>
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            startIcon={<Edit />}
+                            style={{ marginLeft: "20px", marginRight: "30px" }}
+                        // onClick={() => onClickEdit(params.row)}
+                        >
+                            Edit
+                        </Button>
+                        <IconButton onClick={() => handleClickOpen(params.row._id)}>
+                            <DeleteIcon color="secondary" />
+                        </IconButton>
+                    </>
+                );
+            },
+        },
+    ];
+
     return (
         <div className="viewTable">
-            <ViewDetailsBody columns={columns} rows={supplier} onClickCreate={onClickCreate}/>
+            <ViewDetailsBody columns={columns} rows={supplier} onClickCreate={onClickCreate} />
+            <DialogBoxConfirm
+                open={open}
+                handleClose={handleClose}
+                handleClickOpen={handleClickOpen}
+                onClickDelete={onClickDelete}
+            />
         </div>
     );
 }
--- src/pages/supplyItem/ViewSupplyItem.js
+++ src/pages/supplyItem/ViewSupplyItem.js
@@ -6,57 +6,23 @@ import ViewDetailsBody from "../../components/viewDetailsBody/ViewDetailsBody";
 import DeleteIcon from "@material-ui/icons/Delete";
 import { Edit } from "@material-ui/icons";
 import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
 import { useHistory } from "react-router-dom";
+import DialogBoxConfirm from "../../components/dialogBoxConfirm/DialogBoxConfirm";
 
-const columns = [
-    // { field: "_id", headerName: "ID", width: 160 },
-    {
-        field: "item_name",
-        headerName: "Name",
-        width: 150,
-        editable: true,
-    },
-    {
-        field: "unit_price",
-        headerName: "Unit Price",
-        width: 150,
-        editable: true,
-    },
-    {
-        field: "desc",
-        headerName: "Description",
-        width: 150,
-        editable: true,
-    },
-    {
-        field: "action",
-        headerName: "Action",
-        width: 200,
-        editable: true,
-        renderCell: (params) => {
-            return (
-                <>
-                    <Button
-                        variant="contained"
-                        color="primary"
-                        startIcon={<Edit />}
-                        style={{ marginLeft: "20px", marginRight: "30px" }}
-                    >
-                        Edit
-                    </Button>
-                    <DeleteIcon color="secondary" />
-                </>
-            );
-        },
-    },
-];
 
 export default function ViewSupplyItem() {
+
     const [supplyItem, setsupplyItem] = useState([]);
+    const [open, setOpen] = React.useState(false);
+    const [tableID, setTableID] = useState("");
+
+    const [editFormOpen, setEditFormOpen] = useState(false);
+    const [table, setTable] = useState(supplyItem);
 
     useEffect(() => {
         getSupplyDetails();
-    }, []);
+    }, [setOpen,setEditFormOpen]);
 
     const getSupplyDetails = () => {
         axios.get("http://localhost:8000/api/admin/supply-item").then((res) => {
@@ -70,11 +36,96 @@ export default function ViewSupplyItem() {
         history.push("/addsupplyItem")
     }
 
-return (
-    <div className="viewTable">
-        <ViewDetailsBody columns={columns} rows={supplyItem} onClickCreate={onClickCreate} />
-        {/* <Popup openPopup={true} title="Add new table" form={<TableForm />} /> */}
-    </div>
-);
-    }
+    const onClickEdit = (table) => {
+        setEditFormOpen(true);
+        console.log(table);
+        setTable(table);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+    const handleClickOpen = (tableID) => {
+        setTableID(tableID);
+        setOpen(true);
+    };
+
+    const onClickDelete = () => {
+        console.log(tableID);
+        axios
+            .delete("http://localhost:8000/api/admin/supply-item/" + tableID)
+            .then((res) => {
+                console.log("deleted");
+                setOpen(false);
+            });
+    };
+
+    const columns = [
+        // { field: "_id", headerName: "ID", width: 160 },
+        {
+            field: "item_name",
+            headerName: "Name",
+            width: 150,
+            editable: false,
+        },
+        {
+            field: "unit_price",
+            headerName: "Unit Price",
+            width: 150,
+            editable: false,
+        },
+        {
+            field: "desc",
+            headerName: "Description",
+            width: 150,
+            editable: false,
+        },
+        {
+            field: "action",
+            headerName: "Action",
+            width: 200,
+            editable: false,
+            renderCell: (params) => {
+                return (
+                    <>
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            startIcon={<Edit />}
+                            style={{ marginLeft: "20px", marginRight: "30px" }}
+                            onClick={() => onClickEdit(params.row)}
+                        >
+                            Edit
+                        </Button>
+                        <IconButton onClick={() => handleClickOpen(params.row._id)}>
+                            <DeleteIcon color="secondary" />
+                        </IconButton>
+                    </>
+                );
+            },
+        },
+    ];
+
+    return (
+        <div className="viewTable">
+            <ViewDetailsBody columns={columns} rows={supplyItem} onClickCreate={onClickCreate} />
+            {/* <Popup openPopup={true} title="Add new table" form={<TableForm />} /> */}
+            <DialogBoxConfirm
+                open={open}
+                handleClose={handleClose}
+                handleClickOpen={handleClickOpen}
+                onClickDelete={onClickDelete}
+            />
+            {editFormOpen && (
+                <Popup
+                    openPopup={true}
+                    title="Add new table"
+                    form={
+                        <TableForm table={table} buttonTitle="Update"/>
+                    }
+                />
+            )}
+        </div>
+    );
+}
 
